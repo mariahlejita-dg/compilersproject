e701cecfc6a7a6ea5bf3b751a4c2eb17d929b72d
Fabio Augusto Vanegas Bovea <favanegasb@uqvirtual.edu.co>
2020-11-20 00:10:26 -0500

    Arreglado clases secundarias

--- src/co/edu/uniquindio/compiladores/lexico/sintaxis/SentenciaIncremento.kt
+++ src/co/edu/uniquindio/compiladores/lexico/sintaxis/SentenciaIncremento.kt
@@ -1,4 +1,27 @@
 package co.edu.uniquindio.compiladores.lexico.sintaxis
 
+import co.edu.uniquindio.compiladores.lexico.lexico.Token
+import javafx.scene.control.TreeItem
+import java.util.ArrayList
+import javax.swing.tree.DefaultMutableTreeNode
+
 class SentenciaIncremento {
+    private var identificadorVariable: Token? = null
+    private var incremento: Token? = null
+
+    constructor(identificadorVariable: Token, incremento: Token ){
+        this.identificadorVariable = identificadorVariable
+        this.incremento = incremento
+    }
+
+    val arbolVisual: TreeItem<String>
+        get() {
+            val raiz = TreeItem("Sentencia incremento")
+            raiz.children.add(TreeItem<String>(identificadorVariable!!.lexema))
+            raiz.children.add(TreeItem<String>(incremento!!.lexema))
+            return raiz
+        }
+
+
+
 }
--- src/co/edu/uniquindio/compiladores/lexico/sintaxis/Si.kt
+++ src/co/edu/uniquindio/compiladores/lexico/sintaxis/Si.kt
@@ -1,4 +1,45 @@
 package co.edu.uniquindio.compiladores.lexico.sintaxis
 
+import co.edu.uniquindio.compiladores.lexico.lexico.Token
+import javafx.scene.control.TreeItem
+import java.util.ArrayList
+
+
 class Si {
+    private var si: Token? = null
+    var expresion: ExpresionRelacional? = null
+    var listaSentencias: ArrayList<Sentencia>? = null
+
+    constructor(si: Token, expresion: ExpresionRelacional) : super() {
+        this.si = si
+        this.expresion = expresion
+    }
+
+    /**
+     * @param si
+     *
+     * @param expresion
+     *
+     * @param listaSentencias
+     */
+    constructor(si: Token, expresion: ExpresionRelacional,listaSentencias: ArrayList<Sentencia>?
+    ) : super() {
+        this.si = si
+        this.expresion = expresion
+        this.listaSentencias = listaSentencias
+    }
+
+
+
+    val arbolVisual: TreeItem<String>
+        get() {
+            val raiz = TreeItem("Si")
+            raiz.children.add(expresion!!.getArbolVisual())
+            if (listaSentencias != null) {
+                for (sentencia in listaSentencias!!) {
+                    raiz.children.add(sentencia.getArbolVisual())
+                }
+            }
+            return raiz
+        }
 }
