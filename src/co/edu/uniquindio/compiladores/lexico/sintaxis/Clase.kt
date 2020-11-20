2ac67e6f4a10f2290840c3e0ed50d6fda7078577
mariahlejita-dg <alejandra060411@outlook.com>
2020-11-20 00:12:23 -0500

    1. completo hasta Metodo.kt

--- src/co/edu/uniquindio/compiladores/lexico/sintaxis/Clase.kt
+++ src/co/edu/uniquindio/compiladores/lexico/sintaxis/Clase.kt
@@ -3,13 +3,29 @@ package co.edu.uniquindio.compiladores.lexico.sintaxis
 import co.edu.uniquindio.compiladores.lexico.lexico.Token
 import javafx.scene.control.TreeItem
 
-class Clase (var modificador : Token, var palabra_reservada_clase : Token, var identificador_clase : Token,
-             var cuerpoClase : CuerpoClase ) {
+class Clase  {
+    private var modificador : Token ?= null
+    var palabra_reservada_clase : Token ?= null
+    var identificador_clase : Token ?= null
+    var cuerpoClase : CuerpoClase ?= null
+
+    constructor()
+    constructor(modificador : Token, palabra_reservada_clase:Token, identificador_clase: Token, cuerpoClase: CuerpoClase):this() {
+        this.modificador = modificador
+        this.palabra_reservada_clase = palabra_reservada_clase
+        this.identificador_clase = identificador_clase
+        this.cuerpoClase = cuerpoClase
+    }
+    constructor(modificador : Token, palabra_reservada_clase: Token, identificador_clase: Token):this() {
+        this.modificador = modificador
+        this.palabra_reservada_clase = palabra_reservada_clase
+        this.identificador_clase = identificador_clase
+    }
     fun getArbolVisual() : TreeItem<String>{
         val raiz = TreeItem("Clase")
         if(cuerpoClase != null){
-            raiz.children.add( TreeItem(identificador_clase.lexema))
-            raiz.children.add( cuerpoClase.getArbolVisual() )
+            raiz.children.add( TreeItem(identificador_clase!!.lexema))
+            raiz.children.add( cuerpoClase!!.getArbolVisual() )
             return raiz
         }
         return raiz
